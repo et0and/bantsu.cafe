de6ade88cb3e31be3f88cd12a66ed1873375e703
Elliott Cost <elliottcost@gmail.com>
2020-03-14 21:32:39 -0400

    lights fix

--- public/js/lights.js
+++ public/js/lights.js
@@ -3,21 +3,14 @@ fetch('/light-status')
 .then(response => response.json())
 .then(data => {
     console.log(data[0].on);
+    var link = document.querySelector('#lights-off');
     if (data[0].on == 'false') {
-        var link = document.createElement("link");
-        link.href = "./css/lights-off.css";
-        link.type = "text/css";
-        link.rel = "stylesheet";
-        link.media = "screen";
-        link.id = "lights-off";
-
-        document.getElementsByTagName("head")[0].appendChild(link);
+        link.href = "/css/lights-off.css";
+        
         console.log('lights off');
     } else {
-        if (document.querySelector("#lights-off")) {
-            document.querySelector("#lights-off").remove();
-            console.log('lights on');
-        }
+        link.href = "";
+        console.log('lights on');
     }
 })
 .catch(error => console.error(error))
